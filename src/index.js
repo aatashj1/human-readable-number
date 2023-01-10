b9494db3f4a21c6cc942afb8259457c7ab3390a2
aatashj1 <frnkxoxo69@gmail.com>
2023-01-11 00:22:11 +0300

    test done

--- src/index.js
+++ src/index.js
@@ -1,28 +1,47 @@
-module.exports = function toReadable (n) {
-    let a = [
-        '', 'one', 'two', 'three', 'four',
-        'five', 'six', 'seven', 'eight', 'nine',
-        'ten', 'eleven', 'twelve', 'thirteen', 'fourteen',
-        'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
-    ];
-    let b = [
-        '', '', 'twenty', 'thirty', 'forty',
-        'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
-    ];
-
-    let makeGroup = ([ones,tens,huns]) => {
-        return [
-            num(huns) === 0 ? '' : a[huns] + ' hundred ',
-            num(ones) === 0 ? b[tens] : b[tens] && b[tens] + '-' || '',
-            a[tens+ones] || a[ones]
-        ].join('');
-    };
-    if (typeof n === 'number') return numToWords(String(n));
-    if (n === '0')             return 'zero';
-    return comp (chunk(3)) (reverse) (arr(n))
-        .map(makeGroup)
-        .filter(comp(not)(isEmpty))
-        .reverse()
-        .join(' ');
+module.exports = function toReadable(number) {
+    if (number === 0) {
+        return "zero";
+    }
+    if (number <= 9) {
+        return Get0(number);
+    }
+    if (number <= 19) {
+        return Get1(+(number.toString()[1]));
+    }
+    if (number <= 99) {
+        let x10 = Get10(+(number.toString()[0]));
+        let x1 = Get0(+(number.toString()[1]));
+        if (number % 10 === 0) {
+            return x10;
+        }
+        return x10 + " " + x1;
+    }
+    if (number <= 999) {
+        let x100 = Get0(+(number.toString()[0]));
+        let x10andx1 = toReadable(number % 100);
+        if (number % 100 === 0) {
+            return x100 + " hundred";
+        }
+        return x100 + " hundred " + x10andx1;
+    }
 };
+
+let simpleNumbers = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+let strongNumbers = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+let rareNumbers = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+
+function Get0(number) {
+    if (number <= 9) {
+        return simpleNumbers[number];
+    }
 }
+
+function Get1(number) {
+    return strongNumbers[number];
+}
+
+function Get10(number) {
+    return rareNumbers[number]
+}
+
