0a20a339e83e11de2c874550505f58c4a77a1a1d
aatashj1 <frnkxoxo69@gmail.com>
2023-01-10 20:20:32 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable (n) {
+    let a = [
+        '', 'one', 'two', 'three', 'four',
+        'five', 'six', 'seven', 'eight', 'nine',
+        'ten', 'eleven', 'twelve', 'thirteen', 'fourteen',
+        'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
+    ];
+    let b = [
+        '', '', 'twenty', 'thirty', 'forty',
+        'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
+    ];
+
+    let makeGroup = ([ones,tens,huns]) => {
+        return [
+            num(huns) === 0 ? '' : a[huns] + ' hundred ',
+            num(ones) === 0 ? b[tens] : b[tens] && b[tens] + '-' || '',
+            a[tens+ones] || a[ones]
+        ].join('');
+    };
+    if (typeof n === 'number') return numToWords(String(n));
+    if (n === '0')             return 'zero';
+    return comp (chunk(3)) (reverse) (arr(n))
+        .map(makeGroup)
+        .filter(comp(not)(isEmpty))
+        .reverse()
+        .join(' ');
+};
 }
